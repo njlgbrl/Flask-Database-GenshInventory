1d96555ba7fad58dc5aaeddce978baf4e16cb8ab
njlgbrl <ag.cabezas0@gmail.com>
2020-10-30 13:33:02 +0800

    Database transfer from MySQL to SQLite3

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 class Config:
     SECRET_KEY = 'genshinventory{}1234%613869@(#^!@#(^@(*$*!@#(!@#@!%_'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:''@localhost/flaskUsers'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/tako/Python Workspace/Flask/CPE011Database/db/users.db'
     SQLALCHEMY_BINDS = {
-        'itemPool':'mysql+pymysql://root:''@localhost/finalProjDB'
+        'itemPool':'sqlite:////home/tako/Python Workspace/Flask/CPE011Database/db/itempool.db'
     }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
